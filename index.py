28afc956682810cbd0ba453668901ac7ff176377
rzatalibov555 <sethubpro@gmail.com>
2023-02-15 20:16:00 +0400

    Triangle 3 tereflerinin cemini hesablamaq

--- index.py
+++ index.py
@@ -491,3 +491,28 @@ thislist2 = ["Fuad", "Rza", "Sebuhi"]
 
 
 # ========================= EXERCISE  ============================== Palindrome
+
+
+class Triangle:
+
+    def __init__(self, angle1, angle2, angle3):
+        self.angle1 = angle1
+        self.angle2 = angle2
+        self.angle3 = angle3
+
+    number_of_sides = 3
+
+    def check_angles(self):
+        
+        total_ = self.angle1+self.angle2+self.angle3
+        if total_ == 180:
+            return total_
+        else:
+            return False
+        
+
+
+my_triangle = Triangle(90,30,60)
+
+print(my_triangle.number_of_sides)
+print(my_triangle.check_angles())
