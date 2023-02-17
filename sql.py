746cfbb2209694fd3fa4e6f677c17fa8fc78a49f
rzatalibov555 <sethubpro@gmail.com>
2023-02-17 20:15:25 +0400

    SQL creat table

---
+++ sql.py
@@ -0,0 +1,41 @@
+
+# select distinct name, age from autor    // distinct - dublikatlari gosterme
+
+# create table Player (
+# 	name varchar(255),
+# 	surname varchar(255),
+# 	position varchar(255),
+# 	age int,
+# 	cup_count int,
+# 	point int
+# )
+
+
+
+# select * from Player
+
+
+
+# INSERT INTO Player
+# VALUES ('Rahim', 'Mammadov', 'demo3', 23, 5, 99)
+
+
+
+# select name from Player
+
+
+
+# select * from Player where name = 'Rza' AND surname = 'Talibov' or  age = '31'
+# select * from Player where (name = 'Rza' AND surname = 'Talibov') or  age = '31'
+
+
+
+# select cup_count from Player order by name asc
+
+
+
+# select * from Player where (position = 'Rza' AND cup_count = 'Talibov') order by point desc
+
+
+
+# select * from Player where (position = 'demo3' AND cup_count = '5') order by point desc
