a20e87643bd1ececa9b244657eb6d6b6abe929fa
rzatalibov555 <sethubpro@gmail.com>
2023-02-18 21:04:17 +0400

    Employee overtime mmaw hesablamaq

---
+++ exercise.py
@@ -0,0 +1,41 @@
+# class Restaurant:
+#     def __init__(self, menu_items, book_table, customer_order):
+#         menu_items = self.menu_items
+#         book_table = self.book_table
+#         customer_order = self.customer_order
+
+#     def add_item_to_menu_book():
+#         ...
+
+#     def book_tables():
+#         ...
+
+# ================================================================
+
+
+# class Employee:
+#     def __init__(self, emp_id, emp_name, emp_salary, emp_department):
+#         self.emp_id = emp_id
+#         self.emp_name = emp_name
+#         self.emp_salary = emp_salary
+#         self.emp_department = emp_department
+    
+    
+
+#     def calculate_emp_salary(self, hours_worked, emp_salary):
+#         overtime = hours_worked - 50
+#         overtime_amount = (overtime * (emp_salary / 50))
+#         return overtime_amount + self.emp_salary
+        
+
+#     def emp_assign_department(self,department):
+#         self.emp_department = department
+#         return self.emp_department
+#     def print_employee_details(self):
+#         return f"{self.emp_id}\n{self.emp_name}\n{self.emp_salary}\n{self.emp_department}\n"
+
+
+# obj = Employee(1,'Rza',1600,'Coders')
+# print(obj.calculate_emp_salary(24,1600))
+# print(obj.print_employee_details())
+# print(obj.emp_assign_department("IT"))
